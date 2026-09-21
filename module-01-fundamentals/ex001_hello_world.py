442b4d99446e6913459f4ec2aec341b889d11dbd
Dylan Clark <dylanclarkdev2209@gmail.com>
2026-09-20 21:15:45 -0300

    feat: add hello world program

---
+++ module-01-fundamentals/ex001_hello_world.py
@@ -0,0 +1,6 @@
+print('Hello, World!')
+
+# Exercise 001: Hello World in Python
+# My very first Python program as I begin my journey into software development.
+## Description
+# A simple program that prints "Hello, World!" to the screen, learning the basic `print()` syntax.
