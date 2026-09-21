6e505b9c29c11205a0f358d79966b8ab5cc23b97
Dylan Clark <dylanclarkdev2209@gmail.com>
2026-09-21 10:23:56 -0300

    feat: add successor and predecessor calculator script

---
+++ module-01-fundamentals/ex005_successor_predecessor.py
@@ -0,0 +1,5 @@
+# Receive an integer number from the user via keyboard input
+num = int(input('Enter a number: '))
+
+# Analyze the number and display its predecessor and sucessor
+print('Analyzing the number {}, its predecessor is {} and its sucessor is {}'.format(num, (num - 1), (num + 1)))
